0ea2b5d409036e39e8dcf7281e3f87f06372c62d
Tanatip Boonloy <Tanatip Boonloy>
2016-08-16 00:43:48 +0700

    project structure

--- app/src/main/java/kmitl/ce/smart_music_player/ui/PlaylistActivity.java
+++ app/src/main/java/kmitl/ce/smart_music_player/ui/PlaylistActivity.java
@@ -1,13 +1,15 @@
-package kmitl.ce.smart_music_player;
+package kmitl.ce.smart_music_player.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import kmitl.ce.smart_music_player.R;
+
+public class PlaylistActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_playlist);
     }
 }
